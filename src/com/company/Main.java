5d3201c632123b4d364be323ff28f33a39e94b99
TIZIANO.MANFREDI <TIZIANO.MANFREDI@buonarroti.tn.it>
2022-11-16 15:17:11 +0100

    feat: creazione dei due mostri

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,54 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String nome;
+        int vita, danno;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Creazione primo mostro");
+        System.out.println("---");
+        System.out.print("Inserisci il nome del mostro: ");
+        nome = input.nextLine();
+        do {
+            System.out.print("Inserisci la vita del mostro: ");
+            vita = input.nextInt();
+        } while (vita < 1);
+
+        do {
+            System.out.print("Inserisci il danno del mostro: ");
+            danno = input.nextInt();
+        } while (danno < 1);
+
+        Monster m1 = new Monster(nome,vita, danno);
+
+        System.out.println("\n---");
+        System.out.println("Fine creazione primo mostro");
+
+
+        System.out.println("Creazione secondo mostro");
+        System.out.println("---");
+        System.out.print("Inserisci il nome del mostro: ");
+        nome = input.nextLine();
+        do {
+            System.out.print("Inserisci la vita del mostro: ");
+            vita = input.nextInt();
+        } while (vita < 1);
+
+        do {
+            System.out.print("Inserisci il danno del mostro: ");
+            danno = input.nextInt();
+        } while (danno < 1);
+
+        Monster m2 = new Monster(nome,vita, danno);
+
+        System.out.println("\n---");
+        System.out.println("Fine creazione secondo mostro");
+
     }
 }
