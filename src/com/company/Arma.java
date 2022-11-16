54b4287e426426df89a5709a40defa5de7378f65
TIZIANO.MANFREDI <TIZIANO.MANFREDI@buonarroti.tn.it>
2022-11-16 15:59:11 +0100

    feat: creato costruttore di copia e getters nella classe arma

--- src/com/company/Arma.java
+++ src/com/company/Arma.java
@@ -8,4 +8,18 @@ public class Arma {
         this.danno = danno;
         this.nome = nome;
     }
+
+    //costruttore di copia
+    public Arma(Arma a) {
+        this.danno = a.danno;
+        this.nome = a.nome;
+    }
+
+    public int getDanno() {
+        return danno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
