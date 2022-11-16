961e9a5ecd3bacdba5db425d4fb041826d3f35e8
TIZIANO.MANFREDI <TIZIANO.MANFREDI@buonarroti.tn.it>
2022-11-16 15:55:01 +0100

    feat: creato costruttore nella classe arma

--- src/com/company/Arma.java
+++ src/com/company/Arma.java
@@ -3,5 +3,9 @@ package com.company;
 public class Arma {
     private int danno;
     private String nome;
-    
+
+    public Arma(int danno, String nome) {
+        this.danno = danno;
+        this.nome = nome;
+    }
 }
