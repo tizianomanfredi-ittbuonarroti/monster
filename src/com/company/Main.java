c27de1521d96390f254d93466ce8e3a91ef0783d
Tiziano <tiziano.manfredi@buonarroti.tn.it>
2022-11-19 11:10:45 +0100

    feat: creato un menu per raccogliere o abbandonare le armi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         String nome;
-        int vita, danno, nBraccia;
+        int vita, danno, nBraccia, scelta;
 
         Scanner input = new Scanner(System.in);
 
@@ -81,17 +81,88 @@ public class Main {
             System.out.println("---");
             System.out.println("La situazione attuale del mosto 2 è: ");
             System.out.println(m2.toString());
-            System.out.println("\n");
+            System.out.println("---");
 
             if (turno == 1) {
-                m1.subisci(m2.attacca());
-                turno = 2;
+                System.out.println("Tocca al mostro 1");
             } else if (turno == 2) {
-                m2.subisci(m1.attacca());
-                turno = 1;
+                System.out.println("Tocca al mostro 2");
+            }
+
+            do {
+                System.out.println("---");
+                System.out.println("Menù");
+                System.out.println("---");
+                System.out.println("1. Se vuoi attaccare");
+                System.out.println("2. Se vuoi abbandonare un'arma");
+                System.out.println("3. Se vuoi prendere un'arma");
+                System.out.print("Inserisci la tua scelta: ");
+                scelta = input.nextInt();
+            }while(scelta < 0 || scelta > 3);
+
+            switch (scelta){
+                case 1: {
+                    if (turno == 2) {
+                        m1.subisci(m2.attacca());
+                        turno = 1;
+                    } else if (turno == 1) {
+                        m2.subisci(m1.attacca());
+                        turno = 2;
+                    }
+                    break;
+                }
+
+                case 2: {
+                    System.out.print("Inserisci il nome dell'arma: ");
+                    nome = input.nextLine();
+
+
+                    if (turno == 2) {
+                        try {
+                            m2.abbandonaArma(nome);
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    } else if (turno == 1) {
+                        try {
+                            m1.abbandonaArma(nome);
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                    break;
+                }
+
+                case 3: {
+                    System.out.print("Inserisci il nome dell'arma: ");
+                    nome = input.nextLine();
+
+                    do {
+                        System.out.print("Inserisci il danno del mostro: ");
+                        danno = input.nextInt();
+                    } while (danno < 1);
+
+                    Arma a = new Arma(danno, nome);
+
+                    if (turno == 2) {
+                        try {
+                            m2.raccogliArma(a);
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    } else if (turno == 1) {
+                        try {
+                            m1.raccogliArma(a);
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                    break;
+                }
             }
 
             if (m1.vivo() && m2.vivo()) {
+                System.out.println("Lo scontro continua!");
                 continua = true;
             } else if (m1.vivo()) {
                 continua = false;
